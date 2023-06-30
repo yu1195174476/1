42c39c662b46a7bf6361a8d8ae1e480a7c06f14d
liuhuanying <lhying@zhejianglab.com>
2023-06-30 11:21:31 +0800

    删除冗余代码

--- src/config/configuredChains.ts
+++ src/config/configuredChains.ts
@@ -1,17 +1,11 @@
-import EOS from 'src/config/chains/eos';
 import Telos from 'src/config/chains/telos';
-import UX from 'src/config/chains/ux';
-import Wax from 'src/config/chains/wax';
-
-import TelosTestnet from 'src/config/chains/telos-testnet';
-import Jungle from 'src/config/chains/jungle';
 
 import { ChainsConfig } from 'src/types/ChainsConfig';
 import ChainBass from 'src/config/chains/chain-bass';
 
 const chains: ChainsConfig = {
-    mainnets: [new EOS('eos'), new Telos('telos'), new UX('ux'), new Wax('wax'), new ChainBass('chain-bass')],
-    testnets: [new Jungle('jungle'), new TelosTestnet('telos-testnet')],
+    mainnets: [new Telos('telos'), new ChainBass('chain-bass')],
+    testnets: [],
 };
 
 export default chains;
