65d2fe16956aa46b26bff75f99553f5e510a490c
liuhuanying <lhying@zhejianglab.com>
2023-06-30 11:15:39 +0800

    init

--- quasar.conf.js
+++ quasar.conf.js
@@ -95,7 +95,6 @@ module.exports = configure(function (ctx) {
                     target: 'http://localhost:8000',
                     changeOrigin: true,
                     pathRewrite: {
-                        '^/zjchain': ''
                     }
                 }
             }
